3619131b73dbaf40be4f18954403a1f75acc971b
Chris Molis <chrismolis12@gmail.com>
2019-09-03 09:54:54 -0500

    9/3/19 commit

---
+++ src/NauticalMilesInteractive.java
@@ -0,0 +1,10 @@
+public class NauticalMilesInteractive {
+    public static void main(String[] args)
+    {
+        double kilometers;
+        double miles;
+        final double ONE_KILOMETERS = 1.852;
+        final double ONE_MILE = 1.150779;
+        
+    }
+}
